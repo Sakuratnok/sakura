0cb1c42b1e8f7aae4252f74ad97cddcf28220ae5
Sakuratnok <36217326+Sakuratnok@users.noreply.github.com>
2018-02-07 11:18:54 +0700

    Update mybot.php

--- mybot.php
+++ mybot.php
@@ -28,6 +28,13 @@ if (!is_null($events['events'])) {
 					'text' => 'ทดสอบๆ'
 				];
 			}
+			else if($text == 'ced'){
+				// Build message to reply back
+				$messages = [
+					'type' => 'text',
+					'text' => 'Computer Education'
+				];
+			}
 		
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
